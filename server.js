a477bdfb3efc532aac42a4ee43b02641788e4010
Kunal <kunalraghav130@gmail.com>
2026-06-11 02:51:31 +0530

    fix: add missing dependencies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const Job = require('./models/Job');
@@ -8,6 +9,7 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
